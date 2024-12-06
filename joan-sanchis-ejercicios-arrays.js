fb00b422b5ef124c2b103097a758d02bfd170a83
Joan-02 <jsalvadorsb@gmail.com>
2024-12-06 10:33:49 +0100

    Arrays + ejercicio Smiles

--- joan-sanchis-ejercicios-arrays.js
+++ joan-sanchis-ejercicios-arrays.js
@@ -405,3 +405,80 @@ const fruitFound = fruitsComplete.find(function (fruitInfo) {
 });
 
 console.log(fruitFound);
+
+/*wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]*/
+
+function wave(str) {
+  const result = [];
+  const characters = str.split("");
+
+  characters.forEach(function (char, index) {
+    if (char === " ") {
+      return;
+    }
+
+    const charactersCopy = [...characters];
+    charactersCopy[index] = char.toUpperCase();
+    result.push(charactersCopy.join(""));
+  });
+  console.log(result);
+}
+
+return wave("hello");
+
+/*version 2*/
+
+function wave(str) {
+  const result = [];
+  const characters = str.split("");
+
+  characters.forEach(function (char, index) {
+    characters[index] = char.toUpperCase();
+    result.push(characters.join(""));
+    characters[index] = char;
+  });
+  console.log(result);
+}
+
+return wave("hello");
+
+/*Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+Note
+In case of an empty array return 0. You will not be tested with invalid input 
+(input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.*/
+
+
+
+function countSmileys(arr) {
+  
+  const totalSmiles = [0];
+
+  arr.forEach (function(smile){
+    if (smile.includes(':' || ';' && ')' || 'D')) {
+      totalSmiles.push(simle + 1);
+    }
+  })
+
+  return 0;
+
+}
+
+console.log (arr);
+
+
+
